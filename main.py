44a001cea49d7aed3f2b692b5da3080e093c3d05
bignice <wjdrmsl@gmail.com>
2024-04-22 21:23:13 +0900

    Add lbl1 in ui.py to print current date

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication
 
 
 def main():
-    calc = QApplication(sys.argv)
+    # calc = QApplication(sys.argv)
     app = QApplication(sys.argv)
     view = View()
     Control(view=view)
